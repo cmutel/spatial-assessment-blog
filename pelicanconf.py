8a0e422c8ee2294ea08f5fc14a185051b7e390c7
Chris Mutel <cmutel@gmail.com>
2012-12-02 22:38:16 +0100

    Initial revision

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Chris Mutel"
+SITENAME = u"Spatial Assessment"
+SITEURL = ''
+SITESUBTITLE = "weblog of Chris Mutel"
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS =  (('Me@ESD group', 'http://www.ifu.ethz.ch/ESD/people/cmutel'),
+          ('My publications', 'http://scholar.google.ch/citations?user=SJiuf-MAAAAJ'),
+          ('My code', 'https://bitbucket.org/cmutel/'),
+          ('Brightway2', 'http://brightwaylca.org'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+STATIC_PATHS = ['images']
+TYPOGRIFY = True
+THEME = "theme"
+DISPLAY_PAGES_ON_MENU = False
+PLUGINS = ["latex"]
+FEED_ATOM = False
+FEED_RSS = False
---
+++ upload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -zvr output/ 129.132.92.163:Projects/chris.mutel.org/
