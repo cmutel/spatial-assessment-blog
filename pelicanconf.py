161e633c1023eaa9d4405a7f8e39cf5b9ffa208a
Chris Mutel <cmutel@gmail.com>
2026-05-31 12:51:19 +0200

    Fix indentation in LINKS tuple

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ PATH = 'content'
 # Blogroll
 LINKS = (
     ('Notable posts', 'https://chris.mutel.org/tag/notable/index.html'),
-('DdS', 'https://www.d-d-s.ch/'),
+    ('DdS', 'https://www.d-d-s.ch/'),
     ('Brightway', 'https://brightwaylca.org'),
     ('Cauldron', 'https://cauldron.ch/'),
     ('Papers', 'http://scholar.google.ch/citations?user=SJiuf-MAAAAJ'),
